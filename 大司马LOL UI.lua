b3219dccc816e886d462efa15857a1785f7c60a1
ddjlb7598 <q18342117598@163.com>
2025-12-07 20:09:19 +0800

    Update 大司马LOL UI.lua

--- 大司马LOL UI.lua
+++ 大司马LOL UI.lua
@@ -216,7 +216,7 @@ end
       DropShadow.BackgroundTransparency = 1.000
       DropShadow.Position = UDim2.new(0.5, 0, 0.5, 0)
       DropShadow.Size = UDim2.new(1, 10, 1, 10)
-      DropShadow.Image = "rbxassetid://120259348660392" --背景
+      DropShadow.Image = "rbxassetid://128381414495261" --背景
       DropShadow.ImageColor3 = Color3.fromRGB(255,255,255)
       DropShadow.SliceCenter = Rect.new(49, 49, 450, 450)
 
@@ -455,7 +455,7 @@ Open.Draggable = true
 Open.Image = "rbxassetid://113478493756976"
 Open.MouseButton1Click:Connect(function()
   Main.Visible = not Main.Visible
-  Open.Image = Main.Visible and "rbxassetid://128381414495261" or "rbxassetid://120259348660392" --开关的图
+  Open.Image = Main.Visible and "rbxassetid://120259348660392" or "rbxassetid://120259348660392" --开关的图
 end)
 
 UICorner_2.CornerRadius = UDim.new(0, 10)
